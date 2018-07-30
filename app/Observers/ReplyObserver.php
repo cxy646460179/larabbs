cdd36eb1ca3004b872083c12349b836f30dd358d
xiangyu <cxy646460179@gmail.com>
2018-07-30 19:53:20 +0800

    larabbs all

---
+++ app/Observers/ReplyObserver.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Reply;
+use App\Notifications\TopicReplied;
+
+// creating, created, updating, updated, saving,
+// saved,  deleting, deleted, restoring, restored
+
+class ReplyObserver
+{
+    public function created(Reply $reply)
+    {
+        $topic = $reply->topic;
+        $reply->topic->increment('reply_count', 1);
+
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function creating(Reply $reply)
+    {
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function updating(Reply $reply)
+    {
+        //
+    }
+
+    public function deleted(Reply $reply)
+    {
+        if ($reply->topic->reply_count > 0) {
+            $reply->topic->decrement('reply_count', 1);
+        }
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Policies\UserPolicy;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        'App\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
+    }
+}
